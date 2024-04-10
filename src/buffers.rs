47faf10bbc09d3e684d0708d119d93ee0a963432
Ola <ola.owoputi@gmail.com>
2024-04-09 22:10:46 -0400

    add a sliding-window chunked sampler

--- src/buffers.rs
+++ src/buffers.rs
@@ -1,6 +1,6 @@
 use std::{io::{Read, Seek, Write}, marker::PhantomData};
 use hound::{Sample, WavSamples, WavSpec, WavWriter};
-use num_traits::AsPrimitive;
+use num_traits::{AsPrimitive,Float};
 
 pub type ChannelCount = u16;
 pub type SampleRate = u32;
@@ -12,7 +12,7 @@ pub struct SampleConverter<S,F> {
 }
 
 impl<S,F> SampleConverter<S,F>
-where F: AsPrimitive<S>, S:'static+Sample+Copy+AsPrimitive<F> {
+where F: Float+AsPrimitive<S>, S:'static+Sample+Copy+AsPrimitive<F> {
     pub fn new<'wr,R>(_wav_samples: &WavSamples<'wr,R,S>) -> Self {
         Self {
             samp_type: PhantomData::<S>,
@@ -68,22 +68,86 @@ impl<'wr,R,S,F> Iterator for ChunkedSampler<'wr,R,S,F>
 where R:Read, S:Sample+AsPrimitive<F>, F:'static+Copy {
     type Item = SampleBuffer<F>;
     fn next(&mut self) -> Option<Self::Item> {
-        let mut sample_read = false;
         let mut vec = Vec::with_capacity(self.buffer_cap);
         let mut sample: S;
         for _ in 0..self.buffer_cap {
             if let Some(res) = self.sample_iter.next() {
                 sample = res.expect("error while reading sample");
-                sample_read = true;
                 vec.push(sample.as_());
             } else {
                 break // end of iterator
             }
         }
-        if sample_read {
-            Some(SampleBuffer::new(vec, self.wavspec))
-        } else {
+        if vec.is_empty() {
             None
+        } else {
+            Some(SampleBuffer::new(vec, self.wavspec))
+        }
+    }
+}
+
+/*
+ * OverlapSampler: wrapper around WavSamples that loads chunks of samples.
+ * Similar to ChunkedSampler but with an overlapping window
+ */
+pub struct OverlapSampler<'wr,R,S,F> {
+    sample_iter: &'wr mut WavSamples<'wr,R,S>,
+    buffer_cap: usize,
+    step_size: usize,
+    last_chunk: Vec<F>,
+    wavspec: &'wr WavSpec,
+    float_type: PhantomData<F>,
+    samp_type: PhantomData<S>
+}
+
+impl<'wr,R,S,F> OverlapSampler<'wr,R,S,F>
+where R:Read, S:'static+Sample+Copy, F: AsPrimitive<S> {
+    pub fn new(
+        sample_iter: &'wr mut WavSamples<'wr,R,S>,
+        buffer_cap: usize,
+        step_size: usize,
+        wavspec: &'wr WavSpec
+    ) -> Self {
+        assert!(step_size > 0 && step_size <= buffer_cap, "step size must be in range [1, buffer_cap]");
+        let last_chunk = Vec::with_capacity(buffer_cap - step_size);
+
+        Self {
+            sample_iter,
+            buffer_cap,
+            step_size,
+            last_chunk,
+            wavspec,
+            float_type: PhantomData,
+            samp_type: PhantomData
+        }
+    }
+}
+
+impl<'wr,R,S,F> Iterator for OverlapSampler<'wr,R,S,F>
+where R:Read, S:Sample+AsPrimitive<F>, F:'static+Copy {
+    type Item = SampleBuffer<F>;
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut sample: S;
+        
+        let samps_to_load = self.buffer_cap - self.last_chunk.len();
+        let mut vec = Vec::with_capacity(self.buffer_cap);
+        vec.extend(self.last_chunk.drain(0..));
+
+        for _ in 0..samps_to_load {
+            if let Some(res) = self.sample_iter.next() {
+                sample = res.expect("error while reading sample");
+                vec.push(sample.as_());
+            } else {
+                break // end of iterator
+            }
+        }
+
+        self.last_chunk.extend(&vec[self.step_size..]);
+
+        if vec.is_empty() {
+            None
+        } else {
+            Some(SampleBuffer::new(vec, self.wavspec))
         }
     }
 }
